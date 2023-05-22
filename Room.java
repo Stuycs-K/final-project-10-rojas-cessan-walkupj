1b62bc105b7957e9847ce584397bb2190a11d46d
nellirojascessa <nrojascessa40@stuy.edu>
2023-05-22 15:40:27 -0400

    added more variables to constructor

--- Room.java
+++ Room.java
@@ -1,6 +1,11 @@
 public class Room{
   private int decisionValue;
-  public Room(int dV){
-    decisionValue = dV;
+  private String name;
+  private Block[][] blockGrid;
+
+  public Room(int decisionValue, String name){
+    this.decisionValue = decisionValue;
+    this.name = name;
+    blockGrid = new Block[10][10];
   }
 }
