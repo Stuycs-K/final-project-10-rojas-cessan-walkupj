0aef469c7430e3650fba3ff45dac3195ca39028b
nellirojascessa <nrojascessa40@stuy.edu>
2023-05-22 17:02:40 -0400

    framework for classes inventory, player, map

--- Room.java
+++ Room.java
@@ -1,11 +1,11 @@
 public class Room{
   private int decisionValue;
   private String name;
-  private Block[][] blockGrid;
+  //private Block[][] blockGrid;
 
   public Room(int decisionValue, String name){
     this.decisionValue = decisionValue;
     this.name = name;
-    blockGrid = new Block[10][10];
+    //blockGrid = new Block[10][10];
   }
 }
